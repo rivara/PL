467b1d0014810b556ede2045bf1afeada4fb4cfa
rivara <ricardo.v.ramirez@gmail.com>
2019-05-20 22:20:02 +0200

    programa beta final

--- pascalToC2/src/analizador/Main.java
+++ pascalToC2/src/analizador/Main.java
@@ -1,15 +1,19 @@
 package analizador;
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 
 public class Main {
 
-	private static void traductor(ArrayList<Programa> programa, ArrayList<Variable> variables,
+	private static String traductor(ArrayList<Programa> programa, ArrayList<Variable> variables,
 			ArrayList<Constante> constantes, ArrayList<Procedimiento> procedimientos, ArrayList<Funcion> funciones) {
 		String code = null;
 		// directivas
-		code = "#include <stdio.h>/n";
+		code = "#include <stdio.h>\n";
 
 		// variables
 		if (variables.size() != 0) {
@@ -23,96 +27,128 @@ public class Main {
 
 			for (Constante constante : constantes) {
 
-				System.out.println("#define " + constante.identificador + " " + constante.valor);
+				// System.out.println("#define " + constante.identificador + " " +
+				// constante.valor);
+				code = code + "\n#define " + constante.identificador + " " + constante.valor;
 			}
 		}
 
 		// Procedimientos
 		if (procedimientos.size() != 0) {
 			for (Procedimiento procedimiento : procedimientos) {
-				System.out.print(procedimiento.procedimiento);
-				System.out.print(" ");
-				System.out.print(procedimiento.identificador);
+				// System.out.print(procedimiento.procedimiento);
+
+				code = code + procedimiento.procedimiento;
+				// System.out.print(" ");
+				code = code + " ";
+				// System.out.print(procedimiento.identificador);
+				code = code + procedimiento.identificador;
+
 				// FALTA CONTROL DE VACIOS PARA VOID
-				System.out.print("(");
+				// System.out.print("(");
+				code = code + "(";
 				for (FormalParam parametros : procedimiento.formalParam) {
 					String tipo = parametros.tipo;
 					int cont = parametros.identificador.size();
 					for (String ident : parametros.identificador) {
-						System.out.print(tipo);
-						System.out.print(" ");
-						System.out.print(ident);
+						// System.out.print(tipo);
+						code = code + tipo;
+						// System.out.print(" ");
+						code = code + " ";
+						// System.out.print(ident);
+						code = code + ident;
 						cont--;
 						if (cont != 0) {
-							System.out.print(",");
+							// System.out.print(",");
+							code = code + ",";
 						}
 
 					}
 				}
-				System.out.print(")");
+				// System.out.print(")");
+				code = code + ")";
 				// cuerpo procedimeinto
-				System.out.println("{");
+				// System.out.println("{");
+				code = code + "\n{";
 				for (Sent sentencia : procedimiento.bloque.sentlist) {
 					if (sentencia.asig != null) {
-						System.out.print(sentencia.asig);
+						// System.out.print(sentencia.asig);
+						code = code + sentencia.asig;
 					}
 					if (sentencia.pro_call != null) {
-						System.out.print(sentencia.pro_call);
+						// System.out.print(sentencia.pro_call);
+						code = code + sentencia.pro_call;
 					}
 				}
-				System.out.println("}");
+				// System.out.println("}");
+				code = code + "\n}";
 			}
 		}
 
 		// funciones
 		if (funciones.size() != 0) {
 			for (Funcion funcion : funciones) {
-				System.out.print(funcion.tipo);
-				System.out.print(" ");
-				System.out.print(funcion.identificador);
+				// System.out.print(funcion.tipo);
+				code = code + funcion.tipo;
+				// System.out.print(" ");
+				code = code + " ";
+				// System.out.print(funcion.identificador);
+				code = code + funcion.identificador;
 				// FALTA CONTROL DE VACIOS PARA VOID
-				System.out.print("(");
+				// System.out.print("(");
+				code = code + "(";
 				for (FormalParam parametros : funcion.formalParam) {
 					String tipo = parametros.tipo;
 					int cont = parametros.identificador.size();
 					for (String ident : parametros.identificador) {
-						System.out.print(tipo);
-						System.out.print(" ");
-						System.out.print(ident);
+						// System.out.print(tipo);
+						code = code + tipo;
+						// System.out.print(" ");
+						code = code + " ";
+						// System.out.print(ident);
+						code = code + ident;
 						cont--;
 						if (cont != 0) {
-							System.out.print(",");
+							// System.out.print(",");
+							code = code + ",";
 						}
 
 					}
 
 				}
-				System.out.print(")");
-
-				System.out.print(funcion.bloque.begin);
+				// System.out.print(")");
+				code = code + ")";
+				// System.out.print(funcion.bloque.begin);
+				code = code + funcion.bloque.begin;
 				// cuerpo funcion
 				String returno = "";
 				int cont = funcion.bloque.sentlist.size();
 				for (Sent sentencia : funcion.bloque.sentlist) {
 					if (sentencia.asig != null) {
-						System.out.print(sentencia.asig);
+						// System.out.print(sentencia.asig);
+						code = code + sentencia.asig;
 					}
 					if (sentencia.pro_call != null) {
-						System.out.print(sentencia.pro_call);
+						// System.out.print(sentencia.pro_call);
+						code = code + sentencia.pro_call;
 					}
 					// EL ULTIMO SERA de retorno
 					cont--;
 					if (cont == 0) {
 						if (sentencia.asig != null) {
-							System.out.print("retun " + sentencia.asig);
+							// System.out.print("retun " + sentencia.asig);
+							code = code + "retun " + sentencia.asig;
 						}
 						if (sentencia.pro_call != null) {
-							System.out.print("retun " + sentencia.pro_call);
+							// System.out.print("retun " + sentencia.pro_call);
+							code = code + sentencia.pro_call;
 						}
 					}
 				}
-				System.out.print(funcion.bloque.end);
-				System.out.println("");
+				// System.out.print(funcion.bloque.end);
+				code = code + funcion.bloque.end;
+				// System.out.println("");
+				code = code + "\n ";
 			}
 
 		}
@@ -120,22 +156,27 @@ public class Main {
 
 		if (programa.size() != 0) {
 			for (Programa program : programa) {
-				System.out.print("void " + program.identificador + "(void)");
-				System.out.println("{");
+				// System.out.print("void " + program.identificador + "(void)");
+				code = code + "void " + program.identificador + "(void)";
+				// System.out.println("{");
+				code = code + "\n{";
 				// cuerpo
 				for (Sent sentencia : program.bloque.sentlist) {
 					if (sentencia.asig != null) {
-						System.out.print(sentencia.asig);
+						// System.out.print(sentencia.asig);
+						code = code + sentencia.asig;
 					}
 					if (sentencia.pro_call != null) {
-						System.out.print(sentencia.pro_call);
+						// System.out.print(sentencia.pro_call);
+						code = code + sentencia.pro_call;
 					}
 				}
-				System.out.println("}");
+				// System.out.println("}");
+				code = code + "\n}";
 			}
 
 		}
-
+		return code;
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -161,7 +202,18 @@ public class Main {
 			ArrayList<Funcion> funciones = sintaxis.funcion;
 			ArrayList<Programa> programa = sintaxis.programa;
 			// CLASE TRADUCTOR
-			traductor(programa, variables, constantes, procedimientos, funciones);
+			String code = traductor(programa, variables, constantes, procedimientos, funciones);
+			System.out.println(code);
+			// ggenerador de fichero
+			// String str = "World";
+			/*
+			 * BufferedWriter writer = new BufferedWriter(new FileWriter("programa.c",
+			 * true)); writer.append(' '); writer.append(code); writer.close();
+			 */
+			try (Writer writer = new BufferedWriter(
+					new OutputStreamWriter(new FileOutputStream("programa.c"), "utf-8"))) {
+				writer.write(code);
+			}
 
 		}
 
--- pascalToC2/src/analizador/correcto4.pas
+++ pascalToC2/src/analizador/correcto4.pas
@@ -2,7 +2,7 @@ program amigo;
 const amigo = '4';
 const num = 5;
 var algo,cabe : INTEGER;
-var jur : REAL;
+{var jur : REAL;}
 begin
  pruab := 3;
 end
